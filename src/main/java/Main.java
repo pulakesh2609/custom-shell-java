eac739b93331bd2a536b10d9180dec4f7358a231
pulakesh deb roy <pulakesh8721@gmail.com>
2026-08-30 14:34:25 +0530

    #CZ2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.print("$ ");
+
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        System.out.println(input + "command not found");
     }
 }
