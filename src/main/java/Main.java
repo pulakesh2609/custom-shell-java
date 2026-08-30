ed73b5cfb40806c113faa6120eb9a39c92347c50
pulakesh deb roy <pulakesh8721@gmail.com>
2026-08-30 13:52:28 +0530

    #008

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("$");
+        System.out.println("$ ");
     }
 }
