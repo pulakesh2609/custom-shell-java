eaae5ae8b8a02df9933feb69e499b569c6f56527
pulakesh deb roy <pulakesh8721@gmail.com>
2026-08-31 12:14:48 +0530

    #FF0

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,8 +12,8 @@ public class Main {
         while(true){
 
             System.out.print("$ ");
-            String Input = scanner.nextLine();
-            System.out.println(Input + ": commnad not found");
+            String input = scanner.nextLine();
+            System.out.println(input + ": commnad not found");
         }
 
     }
