a68cad982390dc81d26f2bf9993c910ec3519bdc
pulakesh deb roy <pulakesh8721@gmail.com>
2026-08-31 14:39:36 +0530

    #MG5

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 import java.sql.SQLOutput;
 import java.util.Scanner;
+import java.io.File;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -21,14 +22,32 @@ public class Main {
                 if(command.equals("echo") || command.equals("type") || command.equals("exit")){
                     System.out.println(command +" is a shell builtin");
                 }
-                else{
-                    System.out.println(command + " not found");
+
+                else {
+                    String path = System.getenv("PATH");
+                    String[] dirs = path.split(File.pathSeparator);
+                    boolean found = false;
+
+                    for (String dir : dirs) {
+                        File file = new File(dir, command);
+                        if (file.exists() && file.canExecute()) {
+                            System.out.println(command + " is " + file.getPath());
+                            found = true;
+                            break;
+                        }
+                    }
+                    if (!found){
+                            System.out.println(command + ": not found");
+                         }
+                    }
                 }
-            }
             else{
-                    System.out.println(input +": command not found");
+                System.out.println(input +": command not found");
                 }
+
+
         }
+
 
     }
 }
